dc2dda58d16b0fa5b20e5924c941aa070f233f3d
jasonsinclair95 <jason.sinc95@gmail.com>
2024-11-28 07:08:05 +0000

    fix: update providers

--- terraform_modules/lambda/provider.tf
+++ terraform_modules/lambda/provider.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.77.0"
+      version = ">=5.77.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.6.3"
+      version = ">=3.6.3"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = ">=2.6.0"
     }
   }
 }
